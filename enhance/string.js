43819a66072f96ac3eba31cfa735e679d26f5b7b
Rehmat <rehmat@5683.me>
2022-12-29 21:40:04 +0530

    Updated

---
+++ enhance/string.js
@@ -0,0 +1,22 @@
+exports = module.exports = function() {
+  var mod = {
+    start: function() {
+      app.camelCase = function camelize(str, capitalFirst) {
+        if (!app.has(capitalFirst)) capitalFirst = false;
+        var result = str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+          return index === 0 ? word.toLowerCase() : word.toUpperCase();
+        }).replace(/\s+/g, '');
+        if (capitalFirst) result = result.substr(0, 1).toUpperCase() + result.substr(1, 999);
+        return result;
+      };
+      app.properCase = function(str) {
+        return str.replace(
+          /\w\S*/g,
+          function(txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); }
+        );
+      };
+    }
+  };
+  mod.start();
+  return mod;
+}
